255fca90d6c1ca781b8238d4a21b30733ec5686b
rahulkpr2510 <rahulkpr2510@gmail.com>
2026-01-03 14:06:19 +0530

    refactor: make database connection asynchronous
    Use await for connectDB to ensure proper initialization before starting the server

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import "dotenv/config";
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
-connectDB();
+await connectDB();
 app.listen(process.env.PORT || 3000, () => console.log("Server running"));
